c134eb111821716c59a49c9e3b21a0db53401c1f
natnaelh14 <natnaelh14@github.com>
2022-09-20 13:53:08 -0400

    update google tag manager script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app';
 import Layout from 'components/Layout/Layout';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,6 +9,30 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <Script
+        strategy="afterInteractive"
+        id='darkThemeToggle'
+        dangerouslySetInnerHTML={{
+          __html: `
+              if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark')
+            }
+            `
+        }}
+      />
+      {/* Google Tag Manager */}
+      <Script
+        strategy="afterInteractive"
+        id='google-tag-manager'>
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MQ9LW45');`}
+      </Script>
+      {/* End Google Tag Manager */}
     </div>
   )
 }
