9c05131516c88381e5e872206f2757a1a6543203
natnaelh14 <natnaelh14@github.com>
2022-09-22 15:38:06 -0400

    add overview tab styling

--- components/Overview/Overview.tsx
+++ components/Overview/Overview.tsx
@@ -22,14 +22,20 @@ const Overview = () => {
     const { data: coffeeShopsData, error } = useSWR(getUrlForCoffeeShops(latLong, limit), coffeeShopsFetcher);
     const findCoffeeStoreById = coffeeShopsData && coffeeShopsData.find((coffeeShop: CoffeeShopProps) => { return coffeeShop.id === id });
     return (
-        <div className='my-5'>
-            <p className='text-5xl'>{findCoffeeStoreById?.title}</p>
-            <div className='flex flex-row mt-2'>
-                <Ratings />
-                <p className='ml-4 text-sm text-gray-500'>{findCoffeeStoreById?.categories[0]?.name}{" "}{" "}{findCoffeeStoreById?.categories[1]?.name && <span>&#8226;</span>}{" "}{findCoffeeStoreById?.categories[1]?.name && findCoffeeStoreById?.categories[1]?.name}</p>
-            </div>
-            <p className='my-2'>{findCoffeeStoreById?.location}</p>
-        </div>
+        <>
+            {findCoffeeStoreById && (
+                <div className='my-5'>
+                    <p className='text-5xl'>{findCoffeeStoreById?.title}</p>
+                    <div className='flex flex-row mt-2'>
+                        <Ratings />
+                        <p className='ml-4 text-sm text-gray-500'>{findCoffeeStoreById?.categories[0]?.name}{" "}{" "}{findCoffeeStoreById?.categories[1]?.name && <span>&#8226;</span>}{" "}{findCoffeeStoreById?.categories[1]?.name && findCoffeeStoreById?.categories[1]?.name}</p>
+                    </div>
+                    <p className='m-2 text-gray-500 leading-relaxed'>{findCoffeeStoreById?.location}</p>
+                    <p className='m-2 text-gray-500 leading-relaxed'>Located in the heart of Washington, D.C., steps from the National Mall, and across from Pershing Park, a national park home to the new World War 1 Memorial, Café du Parc is reminiscent of a Parisian-inspired sidewalk café.</p>
+                    <p className='m-2 text-gray-500 leading-relaxed'>Paying homage to traditional French cooking, the restaurant offers diners various culinary creations from coffee and pastries at its coffee shop, Le Café, to breakfast, lunch, dinner, and brunch on Sunday on its outside terrace or inside the main dining room.</p>
+                </div>
+            )}
+        </>
     )
 }
 
