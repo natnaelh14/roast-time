29836e23731b5d3c4ea308ee66d47a0de782fdf9
natnaelh14 <natnaelh14@github.com>
2022-11-06 19:18:14 -0500

    enable get all restaurants user local endpoint

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,44 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Item from '../Item/Item';
 import useSWR from 'swr';
-import { coffeeShopsFetcher, getUrlForCoffeeShops, getListOfCoffeeStorePhotos } from '../../libs/coffee-shops';
+import { getListOfCoffeeStorePhotos } from '../../libs/coffee-shops';
 import DashboardLoading from '../Loaders/DashboardLoading';
 
-
 interface CoffeeShopProps {
-    id: string,
-    title: string,
-    categories: {
-        name: string
-    }[],
-    location: string,
-    image: string
+    id: number,
+    restaurant_name: string,
+    // categories: {
+    //     name: string
+    // }[],
+    category: string,
+    restaurant_street_name: string,
+    restaurant_city: string,
+    restaurant_state: string,
+    restaurant_zip_code: string,
+    restaurant_image_url: string
 }
 
 const Dashboard = () => {
-    const latLong = "38.994373%2C-77.029778";
-    const limit = 10;
+    // const latLong = "38.994373%2C-77.029778";
+    // const limit = 10;
 
-    const { data: coffeeShopsData, error } = useSWR(getUrlForCoffeeShops(latLong, limit), coffeeShopsFetcher);
-    console.log("COFFEE SHOPS", coffeeShopsData);
-    const [coffeeShopsPhotos, setCoffeeShopsPhotos] = useState<Array<string>>([])
+    // const { data: coffeeShopsData, error } = useSWR(getUrlForCoffeeShops(latLong, limit), coffeeShopsFetcher);
+    const { data: coffeeShopsData, error } = useSWR(`${process.env.NEXT_PUBLIC_BASE_URL}/restaurants`)
 
     useEffect(() => {
         const fetchData = async () => {
             const photos = await getListOfCoffeeStorePhotos();
             console.log({ photos })
-            setCoffeeShopsPhotos(photos)
         }
         fetchData()
     }, [])
 
     if (!coffeeShopsData && !error) return <DashboardLoading />
+    if (error) return <DashboardLoading />
 
     return (
         <div className="flex flex-row overflow-x-scroll md:overflow-auto md:flex-wrap md:justify-center mt-5">
             { }
-            {coffeeShopsData.map((item: CoffeeShopProps, i: number) => {
-                return <Item key={item.id} id={item.id} title={item.title} image={coffeeShopsPhotos[i]} location={item.location} categories={item.categories} />
+            {coffeeShopsData.map((item: CoffeeShopProps) => {
+                return <Item key={item.id} id={item.id} restaurantName={item.restaurant_name} restaurantImage={item.restaurant_image_url} restaurantStreetName={item.restaurant_street_name} restaurantCity={item.restaurant_city} restaurantState={item.restaurant_state} restaurantZipCode={item.restaurant_zip_code} category={'Cafe'} />
             })}
         </div>
     )
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ['images.unsplash.com', "res.cloudinary.com"],
   },
 }
 
