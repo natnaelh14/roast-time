ab943709884e7a96c12dafd7aa5892c721880686
natnaelh14 <natnaelh14@github.com>
2022-11-13 21:33:37 -0500

    fix testing error

--- components/Users/Users.jsx
+++ components/Users/Users.jsx
@@ -1,23 +1,27 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export const Users = () => {
-    const [users, setUsers] = useState([])
-    const [error, setError] = useState(null)
-    useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/users')
-            .then((res) => setUsers(res.data.map((user) => user.name)))
-            .catch(() => setError('Error fetching users'))
-    }, [])
-    return (
-        <div>
-            <h1>Users</h1>
-            {error && <p>{error}</p>}
-            <ul>
-                {users.map((user) => (
-                    <li key={user}>{user}</li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((res) => setUsers(res.data.map((user) => user.name)))
+        .catch(() => setError("Error fetching users"));
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>Users</h1>
+      {error && <p>{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user}>{user}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- pages/orders.tsx
+++ pages/orders.tsx
@@ -1,12 +1,10 @@
-
-import axios from 'axios';
-import { GetServerSideProps } from 'next';
-import { UserSession } from 'types';
-import { useUserSession } from 'contexts/UserSessionContext';
-import { getSession } from 'components/api/api';
+import axios from "axios";
+import { GetServerSideProps } from "next";
+import { UserSession } from "types";
+import { useUserSession } from "contexts/UserSessionContext";
+import { getSession } from "components/api/api";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-
   // if (!isLoggedIn) {
   //   return {
   //     redirect: {
@@ -16,16 +14,16 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   //   }
   // }
   return {
-    props: {}
-  }
-}
+    props: {},
+  };
+};
 
 const Orders = () => {
-  const { userSession } = useUserSession();
-
   return (
-    <div className='min-h-[600px] flex items-center justify-center text-3xl dark:text-white'>There are no orders yet.</div>
-  )
-}
+    <div className="flex min-h-[600px] items-center justify-center text-3xl dark:text-white">
+      There are no orders yet.
+    </div>
+  );
+};
 
 export default Orders;
