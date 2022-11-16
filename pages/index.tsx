2e0adc429ac382c6afb2347b06f38beb3fd12ea4
natnaelh14 <natnaelh14@github.com>
2022-11-16 18:31:47 -0500

    prettier configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 import Dashboard from 'components/Dashboard/Dashboard';
 import Banner from 'components/Banner/Banner';
 import Adjustment from 'components/Adjustment/Adjustment';
 
 const Home: NextPage = () => {
   return (
-    <div className='mb-20'>
+    <div className="mb-20">
       <Banner />
       <Adjustment />
       <Dashboard />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
