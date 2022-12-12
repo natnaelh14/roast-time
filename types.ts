3f2cc35df27a1183332672e38cf0640e2c32ebcc
natnaelh14 <natnaelh14@github.com>
2022-12-11 22:27:03 -0500

    create save restaurant endpoint

--- types.ts
+++ types.ts
@@ -1,3 +1,4 @@
+import SavedRestaurants from 'pages/restaurant/saved-restaurants';
 import { FieldValues, UseControllerProps } from 'react-hook-form';
 import { ComponentPropsWithoutRef } from 'react';
 
@@ -35,12 +36,18 @@ export interface MenuItemValues {
 }
 
 export interface Restaurant {
-  id: number;
+  id: string;
   name: string;
   address: string;
   category: string;
   imageData: string[];
   userId?: number;
+}
+
+export interface SavedRestaurant {
+  id: string;
+  restaurantId: string;
+  userId: string;
 }
 export interface SessionAccount {
   id: number;
@@ -50,6 +57,7 @@ export interface SessionAccount {
   phoneNumber: string;
   accountType: 'GUEST' | 'RESTAURANT';
   restaurant?: Restaurant;
+  savedRestaurant?: SavedRestaurant;
 }
 export interface UserSession {
   isLoggedIn: boolean;
