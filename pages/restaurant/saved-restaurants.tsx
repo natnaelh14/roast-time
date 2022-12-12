c84cee940337985a0bf65a4ee93326dd7bb5fe9a
natnaelh14 <natnaelh14@github.com>
2022-12-12 17:13:44 -0500

    fetch saved restaurants and display it on the saved restaurant page

--- pages/restaurant/saved-restaurants.tsx
+++ pages/restaurant/saved-restaurants.tsx
@@ -1,10 +1,17 @@
 import { sessionOptions } from 'utils/config';
+import { Restaurant } from 'types';
+import { ThreeDotsLoading } from 'components/Loaders';
+import Item from 'components/Item/Item';
+import EmptyState from 'components/EmptyState/EmptyState';
 import { GetServerSideProps } from 'next';
 import { withIronSessionSsr } from 'iron-session/next';
+import useSWR from 'swr';
 
 export const getServerSideProps: GetServerSideProps = withIronSessionSsr(
   async ({ req, res }) => {
     const { user } = req.session;
+    const accountId = user?.account?.id;
+    const token = user?.token;
 
     if (!user?.isLoggedIn) {
       return {
@@ -15,16 +22,63 @@ export const getServerSideProps: GetServerSideProps = withIronSessionSsr(
       };
     }
     return {
-      props: {},
+      props: {
+        accountId: accountId || null,
+        token: token || null,
+      },
     };
   },
   sessionOptions,
 );
 
-const SavedRestaurants = () => {
+interface SavedRestaurantProps {
+  id: string;
+  userId: string;
+  restaurantId: string;
+  restaurant: Restaurant;
+}
+
+const SavedRestaurants = ({
+  accountId,
+  token,
+}: {
+  accountId: string;
+  token: string;
+}) => {
+  const {
+    data,
+    error,
+    mutate: refreshSavedRestaurants,
+  } = useSWR([
+    `${process.env.NEXT_PUBLIC_BASE_URL}/saved-restaurants/${accountId}`,
+    token,
+  ]);
+  const savedRestaurants = data?.savedRestaurants;
+
+  if (!savedRestaurants && !error) return <ThreeDotsLoading />;
+  if (error) return <EmptyState message="No saved restaurants found" />;
+
   return (
-    <div className="flex min-h-[600px] items-center justify-center text-3xl dark:text-white">
-      This is the saved restaurants page.
+    <div className="my-10 min-h-160 text-3xl dark:text-white">
+      <h1 className="m-4 text-center text-4xl text-brown-dark dark:text-brown-light">
+        Saved Restaurants
+      </h1>
+      <div className="mt-5 flex flex-row overflow-x-scroll md:flex-wrap md:justify-center md:overflow-auto">
+        {savedRestaurants.map((savedRestaurant: SavedRestaurantProps) => {
+          return (
+            <div key={savedRestaurant?.id} className="m-4">
+              <Item
+                id={savedRestaurant?.restaurant.id}
+                name={savedRestaurant?.restaurant.name}
+                imageData={savedRestaurant?.restaurant.imageData}
+                address={savedRestaurant?.restaurant.address}
+                category={savedRestaurant?.restaurant.category}
+                refreshSavedRestaurants={refreshSavedRestaurants}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
